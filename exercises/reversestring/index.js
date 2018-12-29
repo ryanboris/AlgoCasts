5a697b8abdd3d0ff0ac1b140f05bda1fd77fc797
ryanboris <ryan.boris@gmail.com>
2018-12-28 22:58:37 -0500

    soln 3 of reverse

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -9,12 +9,15 @@
 // function reverse(str) {
 //     return [...str].reverse().join('')
 // }
+// function reverse(str) {
+//     let reversed = ''
+//     for (let character of str) {
+//         reversed = character + reversed
+//     }
+//     return reversed //?
+// }
 function reverse(str) {
-    let reversed = ''
-    for (let character of str) {
-        reversed = character + reversed
-    }
-    return reversed
+    return str.split('').reduce((rev, char) => char + rev, '') //?
 }
 reverse('hey there man!')
 module.exports = reverse
